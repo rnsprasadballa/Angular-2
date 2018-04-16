b17176824988892ec5c6999aaad63695cfddf26d
Rama Naga Satya Prasad Balla <rnsprasadballa@users.noreply.github.com>
2018-04-16 11:19:10 +0530

    First Version

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
+
+import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HerosComponent,
+    HeroDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+	FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+//import { HEROES } from '../mockHeros';
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+
+export class HerosComponent implements OnInit {	
+ 
+	hes : Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+selectedHero : Hero;
+  constructor() { }
+
+  ngOnInit() {
+  }
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+}
